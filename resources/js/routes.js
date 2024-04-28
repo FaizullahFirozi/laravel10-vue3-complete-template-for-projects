4d5c8c7892177c514a125a1b180e4ecc930ed086
Faizullah Firozi <faizullah.firozi@gmail.com>
2024-04-28 08:42:18 +0430

    dashboard and rtl
    Dashboard Created and RTL also fixed some worked on routes

---
+++ resources/js/routes.js
@@ -0,0 +1,15 @@
+import Dashboard from './components/Dashboard.vue';
+import Dashboard2 from './components/Dashboard2.vue';
+
+export default [
+    {
+        path: '/dashboard',
+        name: 'dashboard',
+        component: Dashboard,
+    },
+    {
+        path: '/dashboard2',
+        name: 'dashboard2',
+        component: Dashboard2,
+    },
+]
---
+++ resources/views/app.blade.php
@@ -0,0 +1,366 @@
+<!DOCTYPE html>
+<!--if you want to change te Language to English change dir="ltr" -->
+<html lang="en" dir="rtl">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | شروع صفحه</title>
+
+
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{-- @vite(['resources/css/app_rtl.css', 'resources/js/app.js']) --}}
+
+
+</head>
+
+<body class="hold-transition sidebar-mini  layout-fixed">
+    <div class="wrapper" id="app">
+
+        <!-- Navbar -->
+        <nav class="main-header navbar navbar-expand navbar-light">
+            <!-- Left navbar links -->
+            <ul class="navbar-nav">
+                <li class="nav-item" id="toggleMenuIcon">
+                    <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i
+                            class="fas fa-bars"></i></a>
+                </li>
+                <li class="nav-item" id="themeModeFRZ">
+                    <a class="nav-link" href="#" role="button"><i class="mode_icon_frz fas fa-moon"></i></a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="index3.html" class="nav-link">Home</a>
+                </li>
+                <li class="nav-item d-none d-sm-inline-block">
+                    <a href="#" class="nav-link">Contact</a>
+                </li>
+            </ul>
+
+            <!-- left navbar links -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Navbar Search -->
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+                        <i class="fas fa-search"></i>
+                    </a>
+                    <div class="navbar-search-block">
+                        <form class="form-inline">
+                            <div class="input-group input-group-sm">
+                                <input class="form-control form-control-navbar" type="search" placeholder="Search"
+                                    aria-label="Search">
+                                <div class="input-group-append">
+                                    <button class="btn btn-navbar" type="submit">
+                                        <i class="fas fa-search"></i>
+                                    </button>
+                                    <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                                        <i class="fas fa-times"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </li>
+
+                <!-- Messages Dropdown Menu -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="far fa-comments"></i>
+                        <span class="badge badge-danger navbar-badge">3</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
+                        <a href="#" class="dropdown-item">
+                            <!-- Message Start -->
+                            <div class="media">
+                                <img src="dist/img/user1-128x128.jpg" alt="User Avatar"
+                                    class="img-size-50 mr-3 img-circle">
+                                <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                        Brad Diesel
+                                        <span class="float-left text-sm text-danger"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">Call me whenever you can...</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                </div>
+                            </div>
+                            <!-- Message End -->
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <!-- Message Start -->
+                            <div class="media">
+                                <img src="dist/img/user8-128x128.jpg" alt="User Avatar"
+                                    class="img-size-50 img-circle mr-3">
+                                <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                        John Pierce
+                                        <span class="float-left text-sm text-muted"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">I got your message bro</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                </div>
+                            </div>
+                            <!-- Message End -->
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <!-- Message Start -->
+                            <div class="media">
+                                <img src="dist/img/user3-128x128.jpg" alt="User Avatar"
+                                    class="img-size-50 img-circle mr-3">
+                                <div class="media-body">
+                                    <h3 class="dropdown-item-title">
+                                        Nora Silvester
+                                        <span class="float-left text-sm text-warning"><i class="fas fa-star"></i></span>
+                                    </h3>
+                                    <p class="text-sm">The subject goes here</p>
+                                    <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+                                </div>
+                            </div>
+                            <!-- Message End -->
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+                    </div>
+                </li>
+                <!-- Notifications Dropdown Menu -->
+                <li class="nav-item dropdown">
+                    <a class="nav-link" data-toggle="dropdown" href="#">
+                        <i class="far fa-bell"></i>
+                        <span class="badge badge-warning navbar-badge">15</span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-left">
+                        <span class="dropdown-header">15 Notifications</span>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-envelope mr-2"></i> 4 new messages
+                            <span class="float-left text-muted text-sm">3 mins</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-users mr-2"></i> 8 friend requests
+                            <span class="float-left text-muted text-sm">12 hours</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item">
+                            <i class="fas fa-file mr-2"></i> 3 new reports
+                            <span class="float-left text-muted text-sm">2 days</span>
+                        </a>
+                        <div class="dropdown-divider"></div>
+                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+                        <i class="fas fa-expand-arrows-alt"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"
+                        role="button">
+                        <i class="fas fa-th-large"></i>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <aside class="main-sidebar sidebar-dark-indigo elevation-4">
+            <!-- Brand Logo -->
+            <a href="index3.html" class="brand-link">
+                <img src="{{ asset('uploads/AdminLTELogo.png') }}" alt="Logo"
+                    class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">د مدیرت لیکدړه</span>
+            </a>
+
+            <!-- Sidebar -->
+            <div class="sidebar">
+                <!-- Sidebar user panel (optional) -->
+                <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                    <div class="image">
+                        <img src="{{ asset('uploads/user2-160x160.jpg') }}" class="img-circle elevation-2"
+                            alt="Image">
+                    </div>
+                    <div class="info">
+                        <a href="#" class="d-block">فیض الله متقي</a>
+                    </div>
+                </div>
+
+                <!-- SidebarSearch Form -->
+                <div class="form-inline">
+                    <div class="input-group" data-widget="sidebar-search">
+                        <input class="form-control form-control-sidebar" type="search" placeholder="Search"
+                            aria-label="Search">
+                        <div class="input-group-append">
+                            <button class="btn btn-sidebar">
+                                <i class="fas fa-search fa-fw"></i>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Sidebar Menu -->
+                <nav class="mt-2">
+                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                        data-accordion="false">
+                        <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+                        <li class="nav-item">
+                            <router-link to="/dashboard" active-class="active" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard
+                                </p>
+                            </router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link to="/dashboard2" active-class="active" class="nav-link">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Dashboard 2
+                                </p>
+                            </router-link>
+                        </li>
+                        <li class="nav-item menu-open">
+                            <a href="#" class="nav-link active">
+                                <i class="nav-icon fas fa-tachometer-alt"></i>
+                                <p>
+                                    Starter Pages
+                                    <i class="left fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link active">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Active Page</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a href="#" class="nav-link">
+                                        <i class="far fa-circle nav-icon"></i>
+                                        <p>Inactive Page</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-th"></i>
+                                <p>
+                                    Simple Link
+                                    <span class="left badge badge-danger">New</span>
+                                </p>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <!-- /.sidebar-menu -->
+            </div>
+            <!-- /.sidebar -->
+        </aside>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <router-view></router-view>
+
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Control Sidebar -->
+        <aside class="control-sidebar control-sidebar-dark">
+            <!-- Control sidebar content goes here -->
+            <div class="p-3">
+                <h5>Title</h5>
+                <p>Sidebar content</p>
+            </div>
+        </aside>
+        <!-- /.control-sidebar -->
+
+        <!-- Main Footer -->
+        <footer class="main-footer">
+            <!-- To the left -->
+            <div class="float-left d-none d-sm-inline">
+                هر څه دلته شته!
+            </div>
+            <!-- Default to the left -->
+            <strong>د کاپي حقوق &copy; 2024-2024 <a href="https://www.facebook.com/faizullah.firozi">فیض الله
+                    فیروزی</a>.</strong> سره خوندي دي.
+        </footer>
+    </div>
+    <!-- ./wrapper -->
+
+    <script>
+        // Faizullah Firozi. This code is only for ****START**** Collapse the SIDEBAR 
+        document.addEventListener('DOMContentLoaded', () => {
+            const toggleMenuIcon = document.getElementById('toggleMenuIcon');
+
+            const body = document.querySelector('body');
+
+            toggleMenuIcon.addEventListener('click', () => {
+                if (body.classList.contains('sidebar-collapse')) {
+                    localStorage.setItem('sidebarState', 'expanded');
+                } else {
+                    localStorage.setItem('sidebarState', 'collapse');
+                }
+            });
+
+            const sideBarState = localStorage.getItem('sidebarState');
+
+            if (sideBarState === 'collapse') {
+                body.classList.add('sidebar-collapse');
+            }
+        });
+        // Faizullah Firozi. This code is only for ****END**** Collapse the SIDEBAR 
+
+
+
+
+        // Dark Mode theme *****START***** 
+        document.addEventListener('DOMContentLoaded', () => {
+            const themeIcon = document.getElementById('themeModeFRZ');
+            const body = document.querySelector('body');
+            const nav = document.querySelector('nav');
+            const icon = document.querySelector('.mode_icon_frz');
+
+            // Function to apply the theme based on localStorage
+            const applyThemeMode = () => {
+                const themeMode = localStorage.getItem('themeMode');
+                if (themeMode === 'dark') {
+                    body.classList.add('dark-mode');
+                    nav.classList.remove('navbar-light');
+                    nav.classList.add('navbar-dark');
+                    icon.classList.remove('fa-moon');
+                    icon.classList.add('fa-sun');
+
+
+                } else {
+                    body.classList.remove('dark-mode');
+                    nav.classList.remove('navbar-dark');
+                    nav.classList.add('navbar-light');
+                    icon.classList.remove('fa-sun');
+                    icon.classList.add('fa-moon');
+
+
+                }
+            };
+
+            // Event listener for theme mode toggle
+            themeIcon.addEventListener('click', () => {
+                if (body.classList.contains('dark-mode')) {
+                    localStorage.setItem('themeMode', 'light');
+                } else {
+                    localStorage.setItem('themeMode', 'dark');
+                }
+                applyThemeMode(); // Apply the theme after toggling
+            });
+            // Apply the theme mode on page load
+            applyThemeMode();
+        });
+        // Dark Mode theme *****END***** 
+    </script>
+
+</body>
+
+</html>
